af17e1a4fb392ebaf3dc2f7cb2cf3ea6ffda4ff3
Elvis Moses Tangasaiti <elvis.moses@student.moringaschool.com>
2024-03-23 15:33:35 +0300

    creates index.js

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Code your solutions in this file
+// Code your solutions in this file  
+function thankyou(names, gift) {
+    return names.map(name => `Thank you, ${name}, for the wonderful ${gift} gift!`);
+  }
+
+
+function countDown(number) {
+        for (let e = number; e >= 0; e--) {
+          console.log(e);
+        }
+      }
